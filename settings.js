a2ea22a2c5c20fe414abf5d7e6a1896a6af05cd1
pandaliciousxd <167933198+pandaliciousxd@users.noreply.github.com>
2024-12-31 19:05:43 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://keboyeh916:w3b8c07bBUgArIl4@cluster1.viqaz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
